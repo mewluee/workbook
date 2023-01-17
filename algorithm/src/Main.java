536a41805926e42e9d84a2a900a89453e984da8c
mewluee <yunide2@naver.com>
2023-01-17 22:23:54 +0900

    sort패키지

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,94 +1,15 @@
 
 
+import sort.Statistics2108;
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-        int testCase=Integer.parseInt(br.readLine());
-
-
-        //테스트케이스 입력값
-        //1 0 (1개의 데이터, 0번째 있는 데이터의 출력 순서 알려달라)
-        //5 (문서 중요도)
-
-
-        while(testCase>0) {
-            //bw.write("\nwhile문 실행\n");
-            String[] strarr = br.readLine().split(" ");
-            int N = Integer.parseInt(strarr[0]);
-            int M = Integer.parseInt(strarr[1]);
-            //int getCount=Integer.parseInt(strarr[M]);
-
-            Queue<Integer> queue = new LinkedList<Integer>();
-
-            String[] strs = br.readLine().split(" "); // 중요도 받아옴.
-            for (int i = 0; i < N; i++) {
-                queue.add(Integer.parseInt(strs[i])); // 큐에 차근차근 중요도 추가함.
-            }
-            //bw.write(Arrays.toString(queue.toArray())+"\n");
-            //System.out.println("####"+Arrays.toString(queue.toArray()));
-            int count = 0;
-            int index = 0;
-            while (!queue.isEmpty()) {
-                //늘 맨앞을 기준으로 맨앞보다 작으면 뒤로 보낸다?
-                //그런데말이죵 4 1 2 5 면 1일때 2출력해버리면 5가 제일 중요도가 크니까 이런 알고리즘은 안됨.음..
-                //max값을 기준으로 음..LinkedList를 스트림으로 보내서 max값 뽑기 가능.
-
-                //일단 제일 앞에있는 문서를 pop해서 뽑고
-                //나머지 큐에서 max값 출력함.
-                //max값과 문서의 중요도를 비교해서 max값보다 작으면 인쇄안하고 다시 큐에 입력함.
-
-                //count를 도대체 어떻게 세야하지..?
-                //arraylist에다가 0번 인덱스였던게 뒤로가면서 인덱스값이 변하는데
-                //그 인덱스를 추적하는 방법.........
-
-                //index를 기준으로
-                int num = queue.poll(); //제일 왼쪽값(출력) 뽑아옴.
-                //stream으로 max값 뽑는 방법 찾아보기 ㅠ
-//                List<Integer> list=new ArrayList<Integer>((LinkedList<Integer>)queue);
-//                int max=list.stream().mapToInt(x-> (int) x).max().getAsInt();
-                int max = 0;
-                for (Integer i : queue) {
-                    if (max < i) max = i;
-                }
-                //int max=Collections.max(queue.to);
-                //bw.write("max:" + max+"\n");
-
-                if (num < max) { //중요도가 높은 문서가 뒤에 있으니까.
-                    queue.add(num); //다시 큐에 들어가~
-                    if(index==M){//M의 값이 바껴야함.
-                        M=queue.size(); // (제일 뒤로가니까)
-                        index=0; // 다시 해당 M까지 새야함.
-                    }
-                } else {
-                    //num>=max일 경우
-                    count++;
-                    //poll은 위에서 했으니까 따로 처리할 필요 X
-                    if (index == M) { // 출력했는데 해당 index값이 내가 찾아야하는 index값인 경우
-                        bw.write(count + "\n");
-                    }
-                }
-                //뒤로가면..?
-                index++;
-                // 1(*) 1 9 1    1    1     뒤로 입력             0    0
-                // 1    9 1 1    1    1(*)  뒤로 입력             5    1
-                // 9    1 1 1    1(*) 1     여기서는 출력됨.       4    2
-                // 1    1 1 1(*) 1          여기서부터 또 출력      3    3
-
-            }
-
-            testCase--;
-        }
-
-
-
-        bw.close();
-
+        Statistics2108 s=new Statistics2108();
+        s.result();
 
     }
 
---
+++ algorithm/src/sort/Statistics2108.java
@@ -0,0 +1,95 @@
+package sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Statistics2108 {
+
+    private String site="https://www.acmicpc.net/problem/2108";
+
+    public void result() throws IOException {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        int numberCount=Integer.parseInt(br.readLine());
+
+        List<Integer> list=new ArrayList<Integer>();
+
+        for(int i=0; i<numberCount; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        //System.out.println(Arrays.toString(list.toArray()));
+
+        //1.평균
+        //Stream<Double> sd=list.stream();
+        //double average=sd.mapToDouble(x->x).average().getAsDouble();
+        double average=list.stream().mapToDouble(x->x).average().getAsDouble();
+        //System.out.println("**"+average);
+        /*double test=1.76;
+        double test2=1.35;
+        System.out.println(String.format("%.0f  %.0f",test,test2));*/
+        bw.write(Math.round(average)+"\n");
+        //아오..정말..너무...래퍼클래스 싫다..우욱
+
+        //2.중앙값
+        Collections.sort(list);
+        int mid=list.get((list.size()-1)/2);
+        //System.out.println(Arrays.toString(list.toArray()));
+        bw.write(Integer.toString(mid)+"\n");
+
+
+        //3.최빈값
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for(int i=0; i<list.size(); i++){
+            hashMap.put(list.get(i),0); // 초기화
+        }
+        for(int j=0; j<list.size(); j++){
+            int val=hashMap.get(list.get(j));
+            hashMap.put(list.get(j),val+1); // 카운트 증가
+        }
+        // Hashmap으로 중복값도 정리해서
+        // 배열이나 리스트에 담아가지고
+        // value값에 따라 내림차순 정리하고
+        // max값을 찾아서 출력하기
+        // 다음 값도 max값과 동일하면 두번째 값으로 출력하기
+        int[][] arr=new int[hashMap.size()][2];
+        // 값 / 빈도수
+        Iterator<Map.Entry<Integer,Integer>> it=hashMap.entrySet().iterator();
+        int index=0;
+        while(it.hasNext()){
+            Map.Entry<Integer,Integer> entryset=(Map.Entry<Integer, Integer>) it.next();
+            arr[index][0]=entryset.getKey();
+            arr[index][1]=entryset.getValue();
+            index++;
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1]==o2[1]) return o1[0]-o2[0]; // y좌표가 같으면 x좌표 오름차순
+                else return o2[1]-o1[1]; // y좌표 내림차순
+            }
+        });
+        if(arr.length>1){
+            if(arr[0][1]==arr[1][1])
+                bw.write(Integer.toString(arr[1][0])+"\n"); //최빈값이 동일할때 0말고 1인덱스값 출력
+            else
+                bw.write(Integer.toString(arr[0][0])+"\n"); //최빈값이 동일하지 않다면 0인덱스값 출력
+        }else if(arr.length==1){
+            bw.write(Integer.toString(arr[0][0])+"\n"); //개수가 하나라면 0인덱스값 출력
+        }
+
+
+
+
+        //4.범위:최대-최소 차이
+        int max=Collections.max(list);
+        int min=Collections.min(list);
+        int distance=max-min;
+        bw.write(Integer.toString(distance)+"\n");
+
+        bw.close();
+
+    }
+}
