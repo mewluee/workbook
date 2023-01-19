76727c9b2bcebfb0fd0cf62579d47c422ffb4f5e
mewluee <yunide2@naver.com>
2023-01-19 10:50:04 +0900

    스택과 큐 배열로 구현

--- algorithm/src/queue/Queue10845.java
+++ algorithm/src/queue/Queue10845.java
@@ -1,6 +1,7 @@
 package queue;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,6 +9,7 @@ public class Queue10845 {
 
     private String site="https://www.acmicpc.net/problem/10845";
 
+    //큐 linkedlist로 구현/하단에 배열로 구현함
     public void result() throws IOException {
 
         Queue<String> queue=new LinkedList<>();
@@ -45,5 +47,77 @@ public class Queue10845 {
         }
 
         bw.close();
+    }
+
+    //배열로 구현
+    public void result2() throws IOException {
+
+        //Queue<String> queue=new LinkedList<>();
+        int[] queue=new int[]{};
+//        System.out.println(Arrays.toString(queue));
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int numCommand=Integer.parseInt(br.readLine());
+
+        for(int i=0; i<numCommand; i++){
+            String[] strarr=br.readLine().split(" ");
+            String command=strarr[0];
+
+            if(command.equals("push")){
+                //queue.add(strarr[1]); //출력 없음
+                //stack처럼 새로운 배열 만들어서 추가하고 기존 배열로 갱신
+                //System.out.println("입력 전:"+Arrays.toString(queue));
+                int[] sample= Arrays.copyOf(queue, queue.length+1);
+                sample[sample.length-1]=Integer.parseInt(strarr[1]);
+                queue=sample;
+                //System.out.println("입력 후:"+Arrays.toString(queue));
+
+            }else if(command.equals("pop")){
+//                if(queue.isEmpty()) bw.write(Integer.toString(-1)+"\n");
+//                else bw.write(queue.poll().toString()+"\n");
+                //stack이랑 다르게 이동해줘야함.
+                //이번엔 Arrays.copyOfRange를 써서 위치를 지정해서 복사할거임.
+                if(queue.length==0) bw.write(Integer.toString(-1)+"\n");
+                else{
+//                    System.out.println("출력 전:"+Arrays.toString(queue));
+                    int getValue=queue[0]; // stack이랑 다르게 맨 앞(선입선출)값을 출력함
+                    bw.write(Integer.toString(getValue)+"\n");
+                    queue=Arrays.copyOfRange(queue,1,queue.length); // 0번 인덱스빼고 1번 인덱스부터 끝 인덱스전까지 복사
+                    //실제 인덱스는 1부터 queue.length-1 까지 복사됨. 즉 맨끝의 매개변수는 복사 개수가 아니라. 어디까지~ 복사하겠다는 인덱스(그런데 -1함)
+//                    System.out.println("출력 후:"+Arrays.toString(queue));
+                }
+
+            }else if(command.equals("size")){
+//                bw.write(Integer.toString(queue.size())+"\n");
+                bw.write(Integer.toString(queue.length)+"\n");
+
+            }else if(command.equals("empty")){
+//                if(queue.isEmpty()) bw.write(Integer.toString(1)+"\n");
+//                else bw.write(Integer.toString(0)+"\n");
+                if(queue.length==0) bw.write(Integer.toString(1)+"\n");
+                else bw.write(Integer.toString(0)+"\n");
+
+            }else if(command.equals("front")){
+//                if(queue.isEmpty()) bw.write(Integer.toString(-1)+"\n");
+//                else bw.write(queue.peek().toString()+"\n");
+                if(queue.length==0) bw.write(Integer.toString(-1)+"\n");
+                else bw.write(Integer.toString(queue[0])+"\n");
+
+            }else if(command.equals("back")){
+//                if(queue.isEmpty()) bw.write(Integer.toString(-1)+"\n");
+//                else{//스트링 배열로 만들어서 제일 마지막값 출력
+//                    String[] strArrQueue=queue.stream().toArray(String[]::new);
+//                    bw.write(strArrQueue[strArrQueue.length-1]+"\n");
+//                }
+                if(queue.length==0) bw.write(Integer.toString(-1)+"\n");
+                else bw.write(Integer.toString(queue[queue.length-1])+"\n");
+
+            }
+        }
+
+        bw.close();
+
     }
 }
--- algorithm/src/stack/Stack10828.java
+++ algorithm/src/stack/Stack10828.java
@@ -1,12 +1,14 @@
 package stack;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Stack10828 {
 
     private String site="https://www.acmicpc.net/problem/10828";
 
+    //스택으로 구현/하단에 배열로 구현한거 있음.
     public void result() throws IOException {
 
         Stack<String> stack=new Stack<>();
@@ -38,6 +40,77 @@ public class Stack10828 {
 
         bw.close();
 
+    }
+
+    //배열로 구현
+    public void result2 () throws IOException {
+
+        // 배열로 만들기
+        int[] stack=new int[]{}; // []
+        //System.out.println(Arrays.toString(stack));
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int numCommand=Integer.parseInt(br.readLine());
+
+        for(int i=0; i<numCommand; i++){
+            String[] strarr=br.readLine().split(" ");
+            String command=strarr[0];
+
+            if(command.equals("push")){
+                //stack.push(strarr[1]); //출력 없음
+                //System.arraycopy(src배열, scr시작인덱스, dest배열, dest시작인덱스, src복사개수인덱스)
+                //Arrays.copyOf(src배열,복사요소개수) // 위치지정없이 처음부터 복사됨
+                //새배열 만들고
+                //arraycopy로 새배열에 기존배열을 복사해넣고 > 새 값 넣기.
+                int[] sample=Arrays.copyOf(stack,stack.length+1); // 새 배열을 만드는데, 기존 stack배열의 크기보다 +1 크게 만듬. 0으로 채워짐.
+                //System.out.println("입력 전:"+Arrays.toString(stack));
+                sample[sample.length-1]=Integer.parseInt(strarr[1]); // 새로만든 공간에 값을 넣는다.
+                stack=sample; // 기존 stack 배열 갱신
+                //System.out.println("입력 후:"+Arrays.toString(stack));
+                //출력없음
+
+            }else if(command.equals("pop")){
+//                if(stack.isEmpty()) bw.write(Integer.toString(-1)+"\n");
+//                else bw.write(stack.pop().toString()+"\n");
+                // 배열이 비어있으면 -1 출력
+                // 아니면 항상 배열의 마지막 인덱스를 제거한다.
+                // 제거하고 배열의 크기를 줄여준다.
+
+                if(stack.length==0) bw.write(Integer.toString(-1)+"\n"); // 빈 배열이면 -1 출력
+                else{
+                    //System.out.println("출력 전:"+Arrays.toString(stack));
+                    int getValue=stack[stack.length-1];
+                    bw.write(Integer.toString(getValue)+"\n");
+                    stack=Arrays.copyOf(stack,stack.length-1); // 기존 stack 배열 = 새 배열을 만드는데 기존 stack 배열킈 크기보다 -1 작게 복사해서 만듬 > 갱신
+                    //System.out.println("출력 후:"+Arrays.toString(stack));
+
+                }
+
+            }else if(command.equals("size")){
+//                bw.write(Integer.toString(stack.size())+"\n");
+                bw.write(Integer.toString(stack.length)+"\n");
+
+            }else if(command.equals("empty")){
+//                if(stack.isEmpty()) bw.write(Integer.toString(1)+"\n");
+//                else bw.write(Integer.toString(0)+"\n");
+
+                if(stack.length==0) bw.write(Integer.toString(1)+"\n");
+                else bw.write(Integer.toString(0)+"\n");
+
+
+            }else if(command.equals("top")){
+//                if(stack.isEmpty()) bw.write(Integer.toString(-1)+"\n");
+//                else bw.write(stack.peek().toString()+"\n");
+
+                if(stack.length==0) bw.write(Integer.toString(-1)+"\n");
+                else bw.write(Integer.toString(stack[stack.length-1])+"\n");
+
+            }
+        }
+
+        bw.close();
     }
 
 
