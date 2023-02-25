def35b96665cf53068df53172ff387959f8f88f4
mewluee <yunide2@naver.com>
2023-02-25 15:33:53 +0900

    git fix up

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,18 +1,11 @@
 
-
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.regex.Pattern;
+import java.util.*;
 
 public class Main {
 
-
-
     public static void main(String[] args) throws IOException {
-
+        //result();
     }
 
 
