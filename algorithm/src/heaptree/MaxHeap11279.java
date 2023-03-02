f71966e7d64e792d3c61693f5477682cb022e731
mewluee <yunide2@naver.com>
2023-03-02 19:42:03 +0900

    baekjoon

---
+++ algorithm/src/heaptree/MaxHeap11279.java
@@ -0,0 +1,40 @@
+package heaptree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaxHeap11279 {
+
+
+    private String site="https://www.acmicpc.net/problem/11279";
+
+    public static void result2() throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int n = 0; n < N; n++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0) {
+                //제거
+                if(!maxQueue.isEmpty()){
+                    System.out.println(maxQueue.poll());
+                }else{
+                    System.out.println(0);
+                }
+
+            }else{
+                //삽입
+                maxQueue.add(input);
+                //System.out.println("삽입후:"+Arrays.toString(result));
+            }
+        }
+
+    }
+}
