04b631e13894d4e25c1b23aefab9a057d1b53ef2
mewluee <yunide073@gmail.com>
2023-08-07 16:44:16 +0900

    beakjoon 1181

--- algorithm/src/정렬_Sort/단어정렬_1181.java
+++ algorithm/src/정렬_Sort/단어정렬_1181.java
@@ -19,13 +19,14 @@ public class 단어정렬_1181 {
         Arrays.sort(arr, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length()==o2.length()){
-                    for (int i = 0; i < o1.length(); i++) {
+                if(o1.length()==o2.length()){ // 2. 길이가 같으면
+                    for (int i = 0; i < o1.length(); i++) { // 사전 순으로
                         if(o1.charAt(i)!=o2.charAt(i))
                             return o1.charAt(i)-o2.charAt(i);
+                        // 결국 사전순이란 것은, 문자열에서 각 문자들을 비교하면 된다.
                     }
                 }
-                return o1.length()-o2.length();
+                return o1.length()-o2.length(); // 1. 길이가 짧은 것 부터
             }
         });
 
